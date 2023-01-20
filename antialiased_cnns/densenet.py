e24ab90923c9a05caeada8b336b44330229156d8
Vasiliy Adeshkin <adeshkin.vi@gmail.com>
2023-01-20 13:39:40 +0500

    Update densenet.py

--- antialiased_cnns/densenet.py
+++ antialiased_cnns/densenet.py
@@ -114,7 +114,7 @@ class DenseNet(nn.Module):
 
     def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
                  num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000,
-                 filter_size=1, pool_only=True):
+                 filter_size=4, pool_only=True):
 
         super(DenseNet, self).__init__()
 
